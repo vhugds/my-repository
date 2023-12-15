38e351035a0cf813e8977d6c5b2068583341cc61
vhugds <153622281+vhugds@users.noreply.github.com>
2023-12-15 11:58:24 +0100

    practicing video

---
+++ basiccalcx.py
@@ -0,0 +1,39 @@
+#step1: create a folder with the name of the project.e.g(calculator.py)using the .py helps you to know it is a python fil
+#step2: open the folder with your vscode or any other programming app
+#step3: create a file; at the left hand side of ur vscode u see a tool for creating file end ur file with .py to tell vscode it is a python file
+#step4: create a function that adds two numbers together.
+#step4a: create a function with a variable name.
+#step4b: use the return keyword to add two numbers together.
+#step5: create a function that substracts two number together.
+#step5a: create a  function with a variable name.
+#step5b: use the return keyword to substract two numbers together
+#step6: create a function that multiply two numbers together.
+#step6a: create a function with a variable name.
+#step6b: use the return keyword to multiply two numbers together.
+#step7: create a function that divide two numbers together.
+#step7a: create a function with a variable name.
+#step7b: use the return keyword to divide two numbers together.
+#step8: prompt the user to select opration from(add,subtract,divide,multiply).
+#step9: if user select add call the addition function.
+#step9a: initialize two variable num1 and num2.
+#step9b: get values for each of the variable and store them respectively.
+#step9c: add num1 to num2
+#step9d: return the result
+#step10: if user select substract call the substraction function.
+#step10a: initialize two variable num1 and num2.
+#step10b: get values for each of the variable and store them respectively.
+#step10c: add num1 to num2
+#step10d: return the result
+#step11:if user select multiply call the multiplication function.
+#step11a: initialize two variable num1 and num2.
+#step11b: get values for each of the variable and store them respectively.
+#step11c: add num1 to num2
+#step11d: return the result
+#step12: if user select divide call the division function.
+#step12a: initialize two variable num1 and num2.
+#step12b: get values for each of the variable and store them respectively.
+#step12c: add num1 to num2
+#step12d: return the result
+#step13: ask if the user wants more calculation with a yes or no tag.
+#step14: if user selects yes continue the loop from "step7".
+#   else break the loop.
